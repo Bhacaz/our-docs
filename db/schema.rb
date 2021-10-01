df544a5815724d80d55a267a1c6636fc22c83a8c
Jean-Francis Bastien <bhacaz@gmail.com>
2021-10-01 19:47:19 -0400

    Home and login button

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_163019) do
+ActiveRecord::Schema.define(version: 2021_10_01_001955) do
 
   create_table "sites", force: :cascade do |t|
     t.string "repo"
     t.string "branch"
     t.integer "installation_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
